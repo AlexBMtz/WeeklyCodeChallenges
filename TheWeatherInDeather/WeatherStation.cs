6a381346e57f36303af874b70586a898fbae25a3
AlexBMtz <barroetaalejandro160401@gmail.com>
2025-08-28 21:51:57 -0600

    Add Base Code

---
+++ TheWeatherInDeather/WeatherStation.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheWeatherInDeather
+{
+    public class WeatherStation
+    {
+        private Reading reading;
+        private readonly List<DateTime> recordDates = new List<DateTime>();
+        private readonly List<decimal> temperatures = new List<decimal>();
+
+        /// <summary>
+        /// Method to add a new reading and its corresponding date
+        /// </summary>
+        /// <param name="reading">New Reading</param>
+        public void AcceptReading(Reading reading)
+        {
+            this.reading = reading;
+            recordDates.Add(DateTime.Now);
+            temperatures.Add(reading.Temperature);
+        }
+
+        /// <summary>
+        /// Clears all elements from the lists
+        /// </summary>
+        public void ClearAll()
+        {
+            reading = new Reading();
+            recordDates.Clear();
+            temperatures.Clear();
+        }
+
+        public decimal LatestTemperature
+        {
+            get
+            {
+                return reading.Temperature;
+            }
+        }
+
+        public decimal LatestPressure
+        {
+            get
+            {
+                return reading.Pressure;
+            }
+        }
+
+        public decimal LatestRainfall
+        {
+            get
+            {
+                return reading.Rainfall;
+            }
+        }
+
+        public bool HasHistory
+        {
+            get
+            {
+                if (recordDates.Count > 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Outlook ShortTermOutlook
+        {
+            get
+            {
+                if (reading.Equals(new Reading()))
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    if (reading.Pressure < 10m && reading.Temperature < 30m)
+                    {
+                        return Outlook.Cool;
+                    }
+                    else if (reading.Temperature > 50)
+                    {
+                        return Outlook.Good;
+                    }
+                    else
+                    {
+                        return Outlook.Warm;
+                    }
+                }
+            }
+        }
+
+        public Outlook LongTermOutlook
+        {
+            get
+            {
+                if (reading.WindDirection == WindDirection.Southerly
+                    || reading.WindDirection == WindDirection.Easterly
+                    && reading.Temperature > 20)
+                {
+                    return Outlook.Good;
+                }
+                if (reading.WindDirection == WindDirection.Northerly)
+                {
+                    return Outlook.Cool;
+                }
+                if (reading.WindDirection == WindDirection.Easterly
+                    && reading.Temperature <= 20)
+                {
+                    return Outlook.Warm;
+                }
+                if (reading.WindDirection == WindDirection.Westerly)
+                {
+                    return Outlook.Rainy;
+                }
+                throw new ArgumentException();
+            }
+        }
+
+        /// <summary>
+        /// Validates if a reading is not initial
+        /// </summary>
+        /// <returns>Reading state</returns>
+        public State RunSelfTest()
+        {
+            if (reading.Equals(new Reading()))
+            {
+                return State.Bad;
+            }
+            else
+            {
+                return State.Good;
+            }
+        }
+    }
+
+    /*** Please do not modify this struct ***/
+    public readonly struct Reading
+    {
+        public decimal Temperature { get; }
+        public decimal Pressure { get; }
+        public decimal Rainfall { get; }
+        public WindDirection WindDirection { get; }
+
+        public Reading(decimal temperature, decimal pressure,
+            decimal rainfall, WindDirection windDirection)
+        {
+            Temperature = temperature;
+            Pressure = pressure;
+            Rainfall = rainfall;
+            WindDirection = windDirection;
+        }
+    }
+
+    /*** Please do not modify this enum ***/
+    public enum State
+    {
+        Good,
+        Bad
+    }
+
+    /*** Please do not modify this enum ***/
+    public enum Outlook
+    {
+        Cool,
+        Rainy,
+        Warm,
+        Good
+    }
+
+    /*** Please do not modify this enum ***/
+    public enum WindDirection
+    {
+        Unknown, // default
+        Northerly,
+        Easterly,
+        Southerly,
+        Westerly
+    }
+
+}
