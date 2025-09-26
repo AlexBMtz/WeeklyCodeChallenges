2f2be49bbde1cffbff6899a48d7cc56667238b3a
Esaú Bonilla <esafb007@gmail.com>
2025-09-26 13:02:43 -0600

    Reto Completado AEFB

--- International Calling Connoisseur/InternationalCallingConnoisseur.cs
+++ International Calling Connoisseur/InternationalCallingConnoisseur.cs
@@ -1,57 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace International_Calling_Connoisseur
 {
     public static class DialingCodes
     {
+        // Tarea 1
         public static Dictionary<int, string> GetEmptyDictionary()
         {
-            throw new NotImplementedException($"Please implement the (static) GetEmptyDictionary() method");
+            return new Dictionary<int, string>();
         }
 
+        // Tarea 2
         public static Dictionary<int, string> GetExistingDictionary()
         {
-            throw new NotImplementedException($"Please implement the (static) GetExistingDictionary() method");
+            return new Dictionary<int, string>
+            {
+                {1, "United States of America"},
+                {55, "Brazil"},
+                {91, "India"}
+            };
         }
 
+        // Tarea 3
         public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
         {
-            throw new NotImplementedException($"Please implement the (static) AddCountryToEmptyDictionary() method");
+            var dict = new Dictionary<int, string>();
+            dict[countryCode] = countryName;
+            return dict;
         }
 
+        // Tarea 4
         public static Dictionary<int, string> AddCountryToExistingDictionary(
             Dictionary<int, string> existingDictionary, int countryCode, string countryName)
         {
-            throw new NotImplementedException($"Please implement the (static) AddCountryToExistingDictionary() method");
+            // Clonamos para no modificar la referencia original
+            var dict = new Dictionary<int, string>(existingDictionary);
+            dict[countryCode] = countryName;
+            return dict;
         }
 
+        // Tarea 5
         public static string GetCountryNameFromDictionary(
             Dictionary<int, string> existingDictionary, int countryCode)
         {
-            throw new NotImplementedException($"Please implement the (static) GetCountryNameFromDictionary() method");
+            return existingDictionary.ContainsKey(countryCode)
+                ? existingDictionary[countryCode]
+                : string.Empty;
         }
 
+        // Tarea 6
         public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
         {
-            throw new NotImplementedException($"Please implement the (static) CheckCodeExists() method");
+            return existingDictionary.ContainsKey(countryCode);
         }
 
+        // Tarea 7
         public static Dictionary<int, string> UpdateDictionary(
             Dictionary<int, string> existingDictionary, int countryCode, string countryName)
         {
-            throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
+            var dict = new Dictionary<int, string>(existingDictionary);
+            if (dict.ContainsKey(countryCode))
+            {
+                dict[countryCode] = countryName;
+            }
+            return dict;
         }
 
+        // Tarea 8
         public static Dictionary<int, string> RemoveCountryFromDictionary(
             Dictionary<int, string> existingDictionary, int countryCode)
         {
-            throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
+            var dict = new Dictionary<int, string>(existingDictionary);
+            dict.Remove(countryCode);
+            return dict;
         }
 
+        // Tarea 9
         public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
         {
-            throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
+            if (existingDictionary.Count == 0) return string.Empty;
+
+            return existingDictionary.Values
+                .OrderByDescending(name => name.Length)
+                .First();
         }
     }
 }
