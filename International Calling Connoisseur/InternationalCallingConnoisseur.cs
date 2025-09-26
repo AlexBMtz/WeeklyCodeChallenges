34afb5ab5440e3935de904c5535390095a196506
Eric Solis <ericsogo03@gmail.com>
2025-09-26 16:07:31 -0600

    Solucion del reto 1

--- International Calling Connoisseur/InternationalCallingConnoisseur.cs
+++ International Calling Connoisseur/InternationalCallingConnoisseur.cs
@@ -1,57 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace International_Calling_Connoisseur
 {
     public static class DialingCodes
     {
+        /// <summary>
+        /// Clase que retorna un diccionario vacio
+        /// </summary>
+        /// <returns>Diccionario Vacio</returns>
         public static Dictionary<int, string> GetEmptyDictionary()
         {
-            throw new NotImplementedException($"Please implement the (static) GetEmptyDictionary() method");
+            return new Dictionary<int, string>();
         }
-
+        /// <summary>
+        /// Clase que retorna un diccionario pre-llenado
+        /// </summary>
+        /// <returns>Diccionario Pre-llenado</returns>
         public static Dictionary<int, string> GetExistingDictionary()
         {
-            throw new NotImplementedException($"Please implement the (static) GetExistingDictionary() method");
+            return new Dictionary<int, string>
+            {
+                {1, "United States of America" },
+                {55, "Brazil" },
+                {91, "India" }
+            };
         }
-
+        /// <summary>
+        /// Clase que crea un diccionario vacio y le agrega un pais
+        /// </summary>
+        /// <param name="countryCode">Codigo del pais</param>
+        /// <param name="countryName">Nombre del pais</param>
+        /// <returns>Diccionario Con 1 pais</returns>
         public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
         {
-            throw new NotImplementedException($"Please implement the (static) AddCountryToEmptyDictionary() method");
+            var diccionario = GetEmptyDictionary();
+            diccionario[countryCode] = countryName;
+            return diccionario;
         }
 
+
+        /// <summary>
+        /// Metodo que agrega un pais a un diccionario existente
+        /// </summary>
+        /// <param name="existingDictionary">Diccionario al que se agregará</param>
+        /// <param name="countryCode">Codigo del pais</param>
+        /// <param name="countryName">Nombre del pais</param>
+        /// <returns>Diccionario con el pais agregado</returns>
         public static Dictionary<int, string> AddCountryToExistingDictionary(
             Dictionary<int, string> existingDictionary, int countryCode, string countryName)
         {
-            throw new NotImplementedException($"Please implement the (static) AddCountryToExistingDictionary() method");
+            if (!existingDictionary.ContainsKey(countryCode)) // Si el codigo no existe en el diccionario
+            {
+                existingDictionary.Add(countryCode, countryName); //Agregamos
+            }
+            return existingDictionary;
         }
-
+        /// <summary>
+        /// Metodo que retorna el nombre de un pais en base a su codigo
+        /// </summary>
+        /// <param name="existingDictionary">Diccionario existente</param>
+        /// <param name="countryCode">Codigo del pais</param>
+        /// <returns>Nombre del pais o cadena vacia</returns>
         public static string GetCountryNameFromDictionary(
             Dictionary<int, string> existingDictionary, int countryCode)
         {
-            throw new NotImplementedException($"Please implement the (static) GetCountryNameFromDictionary() method");
+            return existingDictionary.TryGetValue(countryCode, out var name) ? name : string.Empty; //Intenta obtener el valor de la clase, si da verdadero retorna el valor, falso retorna cadena vacia
         }
 
+        /// <summary>
+        /// Metodo que revisa si un pais existe dentro del diccionario
+        /// </summary>
+        /// <param name="existingDictionary">Diccionario existente</param>
+        /// <param name="countryCode">Codigo del pais</param>
+        /// <returns>True o False</returns>
         public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
         {
-            throw new NotImplementedException($"Please implement the (static) CheckCodeExists() method");
+            return existingDictionary.ContainsKey(countryCode);
         }
 
+        /// <summary>
+        /// Metodo que actualiza el nombre de un pais en base a la clave proporcionada
+        /// </summary>
+        /// <param name="existingDictionary">Diccionario Existente</param>
+        /// <param name="countryCode">Codigo del pais</param>
+        /// <param name="countryName">Nombre del pais</param>
+        /// <returns>Diccionario Actualizado</returns>
         public static Dictionary<int, string> UpdateDictionary(
             Dictionary<int, string> existingDictionary, int countryCode, string countryName)
         {
-            throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
+            if (existingDictionary.ContainsKey(countryCode)) //Si existe una clave con ese valor
+            {
+                existingDictionary[countryCode] = countryName; //Actualiza el valor asociado a esa clave
+            }
+            return existingDictionary;
         }
 
+        /// <summary>
+        /// Elimina un pais del diccionario
+        /// </summary>
+        /// <param name="existingDictionary">Diccionario existente</param>
+        /// <param name="countryCode">Codigo del pais</param>
+        /// <returns>Diccionario Actualizado</returns>
         public static Dictionary<int, string> RemoveCountryFromDictionary(
             Dictionary<int, string> existingDictionary, int countryCode)
         {
-            throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
+            existingDictionary.Remove(countryCode); //Elimina el valor de la clave especificada
+            return existingDictionary;
         }
-
+        /// <summary>
+        /// Metodo que retorna el pais con nombre mas largo del diccionario
+        /// </summary>
+        /// <param name="existingDictionary">Diccionario existente</param>
+        /// <returns>El nombre del país con m[as caracteres
+        /// Si el diccionario esta vacio retorna cadena vacia</returns>
         public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
         {
-            throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
+            if(existingDictionary.Count == 0) return string.Empty; //Si no hay nada retorna vacio
+            return existingDictionary.Values.OrderByDescending(c => c.Length).First();//Ordenamos el diccionario de mayor a menos segun la longitud y retornamos el primer valor (mas largo)
+
         }
     }
 }
