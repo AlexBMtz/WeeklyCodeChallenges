112e311ebaf7b4d629e24a5968976a3a7505b99b
Bryan Arturo Barraza Garcia <bry.art717@gmail.com>
2025-10-03 11:38:22 -0600

    Updated changes

--- TracksOnTracksOnTracks/Languages.cs
+++ TracksOnTracksOnTracks/Languages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TracksOnTracksOnTracks
 {
@@ -7,47 +8,64 @@ namespace TracksOnTracksOnTracks
     {
         public static List<string> NewList()
         {
-            throw new NotImplementedException("Please implement the static Languages.NewList() method");
+            return new List<string>();
         }
 
         public static List<string> GetExistingLanguages()
         {
-            throw new NotImplementedException("Please implement the static Languages.GetExistingLanguages() method");
+            return new List<string> { "C#", "Clojure", "Elm"};
         }
 
         public static List<string> AddLanguage(List<string> languages, string language)
         {
-            throw new NotImplementedException("Please implement the static Languages.AddLanguage() method");
+            languages.Add(language);
+            return languages;
         }
 
         public static int CountLanguages(List<string> languages)
         {
-            throw new NotImplementedException("Please implement the static Languages.CountLanguages() method");
+            return languages.Count();
         }
 
         public static bool HasLanguage(List<string> languages, string language)
         {
-            throw new NotImplementedException("Please implement the static Languages.HasLanguage() method");
+            return languages.Contains(language);
         }
 
         public static List<string> ReverseList(List<string> languages)
         {
-            throw new NotImplementedException("Please implement the static Languages.ReverseList() method");
+            languages.Reverse();
+            return languages;
         }
 
         public static bool IsExciting(List<string> languages)
         {
-            throw new NotImplementedException("Please implement the static Languages.IsExciting() method");
+            // Si no hay nada pos no
+            if (languages == null || languages.Count == 0 )
+            {
+                return false;
+            }
+
+            // El primero es C#
+            if ( languages[ 0 ] == "C#" )
+                return true;
+
+            // El segundo es C# y hay 2 o 3
+            if ( languages.Count <= 3 && languages.Count >= 2 && languages[ 1 ] == "C#" )
+                return true;
+
+            else return false;
         }
 
         public static List<string> RemoveLanguage(List<string> languages, string language)
         {
-            throw new NotImplementedException("Please implement the static Languages.RemoveLanguage() method");
+            languages.Remove(language);
+            return languages;
         }
 
         public static bool IsUnique(List<string> languages)
         {
-            throw new NotImplementedException("Please implement the static Languages.IsUnique() method");
+            return languages.Distinct().Count() == languages.Count();
         }
     }
 }
